66020a01849758976e942a2af81a5d4f4ea015f1
brain545 <brain545@yandex.ru>
2018-05-05 05:04:00 +0300

    21 completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+YTSearch({ key: process.env.API_KEY, term: 'surfboards' }, function (data) {
+    console.log(data);
+});
 
 const App = () => {
-    return <div>{process.env.API_KEY}</div>;
+    return (
+        <div>
+            <div>{process.env.API_KEY}</div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
